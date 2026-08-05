fc5b80118fcf37256b1df464fd285a2587f1b442
mohamedyousalama-ctrl <mohamed.you.salama@gmail.com>
2026-08-05 03:55:59 +0300

    feat(collab): add isolated conversation desktop route

---
+++ app/(console-v2)/c/(app)/conversation-desktop/page.tsx
@@ -0,0 +1,5 @@
+import { ConversationDesktop } from "@/components/console-v2/conversation-desktop/ConversationDesktop";
+
+export default function ConversationDesktopPage() {
+  return <ConversationDesktop />;
+}
